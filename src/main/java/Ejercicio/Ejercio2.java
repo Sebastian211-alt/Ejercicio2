71c47fda2c1faf4fd51e3e11dfe528accbd82843
Sebastil24kc <85630110+Sebastian211-alt@users.noreply.github.com>
2022-03-15 11:47:34 -0600

    Update Ejercio2.java

--- src/main/java/Ejercicio/Ejercio2.java
+++ src/main/java/Ejercicio/Ejercio2.java
@@ -1,3 +1,8 @@
+/**
+Modifica el programa anterior para que además se muestre tu dirección y
+tu número de teléfono. Asegúrate de que los datos se muestran en líneas
+separadas.
+*/
 
 package Ejercicio;
 
@@ -7,6 +12,7 @@ import java.util.Scanner;
  *
  * @author SebasTati
  */
+
 public class Ejercio2 {
 
     public static void main(String[] args) {
